2f5f0e35981a54ea50c24a758f202f41f2c48d73
manish gautam <manish.gautam@hoogaaa.com>
2023-08-03 23:39:10 +0530

    aws hosting also done

--- all step.js
+++
@@ -1,9 +0,0 @@
-// to host docker iamge on aws ec2;
-// first create ECR and push the docker image in ECR
-// create ec2 instance and connect with terminal;
-// sudo apt-get update 
-// install aws cli 
-// configure aws 
-// loging docker using ECR Credential
-// pull the docker image from ECR 
-// run the docker Image
